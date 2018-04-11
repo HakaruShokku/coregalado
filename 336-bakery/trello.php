f6d6604a6de1572d0d3dc00836d601a1dd26ad9e
Cody Regalado <coregalado@csumb.edu>
2018-04-11 22:00:46 +0000

    retry on team project

---
+++ 336-bakery/trello.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Conchitas! </title>
+        
+            <link rel="stylesheet" href="css/project1.css" type="text/css" />
+    </head>
+    <body>
+        
+        <h1> <b> Welcome to La Conchita! </b></h1>
+<ul class="topnav">
+  <li> <a href="index.php">Home </a> </li>
+   <li> <a href="menu.php">Menu </a>  </li>
+    <li> <a href="trello.php">Trello </a>  </li>
+</ul>
+    
+    <h1>Trello Site</h1>
+    
+   <h2> <a href="https://trello.com/b/JIiAIV9w/bakery-336">Trello</a></h2>
+    
+
+    
+    </br>
+
+
+    </body>
+</html>
